cfad58b83c185881240b2e5bc47c3b3f320a38e5
asik7777 <127174098+asik7777@users.noreply.github.com>
2023-09-11 14:05:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -264,7 +264,7 @@ async def advantage_spoll_choker(bot, query):
                     InlineKeyboardButton("🔰 𝗠𝗼𝘃𝗶𝗲 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 𝗧𝗼 𝗔𝗱𝗺𝗶𝗻 🔰", url=f"https://t.me/Filmymod_Support")
                 ]]
                 k = await query.message.edit(script.MVE_NT_FND, reply_markup=InlineKeyboardMarkup(btn))
-                await asyncio.sleep(15)
+                await asyncio.sleep(10)
                 await k.delete()
 #languages
 
@@ -1879,7 +1879,7 @@ async def advantage_spell_chok(client, msg):
                 caption=script.I_CUDNT.format(mv_rqst),
                 reply_markup=InlineKeyboardMarkup(button)
             )
-            await asyncio.sleep(30)
+            await asyncio.sleep(15)
             await k.delete()
             return
         regex = re.compile(r".*(imdb|wikipedia).*", re.IGNORECASE)  # look for imdb / wiki results
@@ -1917,7 +1917,7 @@ async def advantage_spell_chok(client, msg):
                 caption=script.I_CUDNT.format(mv_rqst),
                 reply_markup=InlineKeyboardMarkup(button)
             )
-            await asyncio.sleep(30)
+            await asyncio.sleep(15)
             await k.delete()
             return
         SPELL_CHECK[mv_id] = movielist
@@ -1961,7 +1961,7 @@ async def advantage_spell_chok(client, msg):
                 caption=script.I_CUDNT.format(mv_rqst),
                 reply_markup=InlineKeyboardMarkup(button)
             )
-            await asyncio.sleep(30)
+            await asyncio.sleep(15)
             await k.delete()
             return
         movielist = []
@@ -1977,7 +1977,7 @@ async def advantage_spell_chok(client, msg):
                 caption=script.I_CUDNT.format(mv_rqst),
                 reply_markup=InlineKeyboardMarkup(button)
             )
-            await asyncio.sleep(30)
+            await asyncio.sleep(15)
             await k.delete()
             return
         movielist += [movie.get('title') for movie in movies]
