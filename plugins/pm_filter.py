1acdc5ac7508aaff73a0a7ca0179e348cbd35ccd
asik7777 <127174098+asik7777@users.noreply.github.com>
2023-09-12 14:40:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1790,7 +1790,7 @@ async def auto_filter(client, msg, spoll=False):
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await dlt.delete()
+            await m.delete()
             try:
                 if settings['auto_delete']:
                     await dlt.delete()
