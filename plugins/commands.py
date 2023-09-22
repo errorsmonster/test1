3cf821e28d284b1e1dde6b43eeed9e7800cea146
asik7777 <127174098+asik7777@users.noreply.github.com>
2023-09-22 20:34:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -87,9 +87,9 @@ async def start(client, message):
                 btn.append([InlineKeyboardButton("🔄 𝗧𝗿𝘆 𝗔𝗴𝗮𝗶𝗻", callback_data=f"{pre}#{file_id}")])
             except (IndexError, ValueError):
                 btn.append([InlineKeyboardButton("🔄 𝗧𝗿𝘆 𝗔𝗴𝗮𝗶𝗻", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
-            chat_id=message.from_user.id,
         await client.send_photo(
             photo="https://telegra.ph/file/9e316ca0512d9478fd771.jpg",
+            chat_id=message.from_user.id,
             text="𝗣𝗹𝗲𝗮𝘀𝗲 ♻️ 𝗝𝗼𝗶𝗻 𝗠𝘆 𝗨𝗽𝗱𝗮𝘁𝗲 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 ♻️ 𝗙𝗶𝗿𝘀𝘁, 𝗧𝗵𝗲𝗻 𝗖𝗼𝗺𝗲 𝗯𝗮𝗰𝗸 𝗛𝗲𝗿𝗲 𝗮𝗻𝗱 𝗖𝗹𝗶𝗰𝗸 🔄 𝗧𝗿𝘆 𝗔𝗴𝗮𝗶𝗻 𝗕𝘂𝘁𝘁𝗼𝗻 ...\n\n𝗣𝗵𝗲𝗹𝗲 𝗠𝗲𝗿𝗲 𝗨𝗽𝗱𝗮𝘁𝗲 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 𝗠𝗲 𝗝𝗼𝗶𝗻 𝗛𝗼 𝗝𝗮𝗼 𝗨𝘀𝗸𝗲 𝗯𝗮𝗮𝗱 𝗧𝗿𝘆 𝗔𝗴𝗮𝗶𝗻 𝗕𝘂𝘁𝘁𝗼𝗻 𝗣𝗲 𝗖𝗹𝗶𝗰𝗸 𝗸𝗮𝗿𝗼 ☟",          
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
