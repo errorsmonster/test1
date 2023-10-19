90ae9e5c60a3eb9c281f1f47c7177e4671e10dbf
asik7777 <127174098+asik7777@users.noreply.github.com>
2023-10-19 12:13:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,8 +55,8 @@ IS_STREAM = bool(STREAM_BIN and STREAM_URL)
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'ziplinker.net')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'a4de59ef1ac0249385ce0d54796c9d0bd4eebb63')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mplaylink.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '1f1da5c9df9a58058w672acw8d8134e203b03426a1')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
